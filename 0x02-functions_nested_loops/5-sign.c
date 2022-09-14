ed03f00512d21edfe03327334e060fa8370c1322
sammy-yuntin <yuntinsammy@yahoo.com>
2022-09-14 11:25:28 -0700

    5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,11 +2,11 @@
 /**
  * print_sign -> prints based on condition
  * @n:argument passed
- * return:1, 0-1
+ * return:1,0 or -1
  */
 int print_sign(int n)
 {
-	if (n>0>
+	if (n>0)
 	{
 	_putchar('+');
 		return (1);
