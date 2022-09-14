bd88f62a919059ae6322fb305d728c25a09e11e6
sammy-yuntin <yuntinsammy@yahoo.com>
2022-09-14 10:47:34 -0700

    s

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,12 +5,11 @@
  **/
 void print_alphabet_x10(void)
 {
-	int j;
- 
-	int x;
-	for (x = 0; x < 10; x++)
+int j;
+int x;
+	for (x=0; x<10; x++)
  		{
-		for (j = 'a'; j < ='z'; j++)
+		for (j='a'; j<='z'; j++)
 			{
 			_putchar(j);
 			}
