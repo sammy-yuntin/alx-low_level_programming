c5dc6f568293a20f7a8527135b339da4d263d5b3
sammy-yuntin <yuntinsammy@yahoo.com>
2022-09-14 10:32:14 -0700

    see

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,17 +3,17 @@
 /**
  * print_alphabet_x10->prints the lowercase alphabets
  **/
- void print_alphabet_x10(void)
- {
- int j;
- int x;
-
- for (x=0; x<10; x++)
- 	{
- 	for (j='a';j<='z';j++)
-		{
-		_putchar(j);
-		}
+void print_alphabet_x10(void)
+{
+	int j;
+ 
+	int x;
+	for (x = 0; x < 10; x++)
+ 		{
+			for (j = 'a'; j < ='z'; j++)
+			{
+			_putchar(j);
+			}
 		_putchar('\n');
-	}
+		}
 }
