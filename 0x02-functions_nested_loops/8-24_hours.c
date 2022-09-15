423eb5ad43309895f84ef9a9eafa874d523806b5
sammy-yuntin <yuntinsammy@yahoo.com>
2022-09-14 23:05:26 -0700

    8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,21 +2,21 @@
 
 /**
  * jack baeur-prints the minutes of a day
- * return: no return
+ * Return: no return
  */
 void jack_bauer(void)
 {
-	int a,b,c,d;
+	int a, b, c, d;
 
 	for (a = '0'; a <= '2'; a++)
 	{
-		for (b = '0';b <= '3'; b++)
+		for (b = '0'; b <= '3'; b++)
 		{
-			for (c = '0';c <= '5';c++)
+			for (c = '0'; c <= '5'; c++)
 			{
 				for (d = '0'; d <= '9'; d++)
 				{
-				if(a <= '2' && b <= '3' && c <= '5' && d <='9')
+				if (a <= '2' && b <= '3' && c <= '5' && d <= '9')
 					break;
 						_putchar(a);
 						_putchar(b);
@@ -26,6 +26,6 @@ void jack_bauer(void)
 						_putchar('\n');
 				}
 			}
-		}	
+		}
 	}
 }
