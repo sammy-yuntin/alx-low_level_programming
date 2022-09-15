fe9c0deebe0f02fa8729d0dece37d3a13bfa2d02
sammy-yuntin <yuntinsammy@yahoo.com>
2022-09-14 22:53:20 -0700

    8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,11 +8,11 @@ void jack_bauer(void)
 {
 	int a,b,c,d;
 
-	for (a = 48; a <= 57; a++)
+	for (a = 48; a <= 50; a++)
 	{
-		for (b = 48;b <= 57; b++)
+		for (b = 48;b <= 51; b++)
 		{
-			for (c = 48;c <= 57;c++)
+			for (c = 48;c <= 53;c++)
 			{
 				for (d = 48; d <= 57; d++)
 				{
