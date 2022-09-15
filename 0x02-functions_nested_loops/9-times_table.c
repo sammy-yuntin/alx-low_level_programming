8d40ddeb0d8850c6e0c7e1d5df535dc1797cbfd1
sammy-yuntin <yuntinsammy@yahoo.com>
2022-09-15 14:42:32 -0700

    9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,10 +12,10 @@ void times_table(void)
 	for(a=0;a<=9;b++)
 	{
 		for(b=1;b<=9;b++)
-			op = a*b;
+		
 		{
-			
-			_putchar(op);
+				
+			_putchar((a*b));
 			_putchar(44);
 			_putchar(32);
 		}	
