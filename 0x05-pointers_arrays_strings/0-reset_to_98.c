9f806108d5edaecaac7286582c65b032d4bb8847
sammy-yuntin <yuntinsammy@yahoo.com>
2022-09-20 07:23:13 -0700

    0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -13,47 +13,7 @@
 void reset_to_98(int *n)
 {
 
-	int n;
-	int *p;
-
-	*p = &n;
+	*n = 98;
 	return (0);
 
 }
-#include <stdio.h>
-
-
-
-void reset_to_98(int *);
-
-
-
-/**
- *
- *  * main - check the code for Holberton School students.
- *
- *   *
- *
- *    * Return: Always 0.
- *
- *     
-
-int main(void)
-
-{
-
-		int n;
-
-
-
-			n = 402;
-
-				printf("%d\n", n);
-
-					reset_to_98(&n);
-
-						printf("%d\n", n);
-
-							return (0);
-
-}**/
