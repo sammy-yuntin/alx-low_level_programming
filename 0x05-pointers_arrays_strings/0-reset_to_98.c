74a2e61592496ea1a20a878f1bce9aa4e46b0262
sammy-yuntin <yuntinsammy@yahoo.com>
2022-09-20 07:47:21 -0700

    0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,10 @@
 /**
- *main - check the code 
- *
- *Return: Always 0.
- *
- **/
+*main - reset_to_98 takes a pointer to an int as parametre and updates it
+* Now, the value in the pointer is 98
+*
+*Return: returns nothing.
+*
+**/
 
 void reset_to_98(int *n)
 {
