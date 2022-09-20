879be51ba96dddef8def425330d70edc47403dbc
sammy-yuntin <yuntinsammy@yahoo.com>
2022-09-20 07:38:02 -0700

    0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,17 +1,14 @@
 /**
  *
- *  * main - check the code 
+ * main - check the code 
  *
- *   *
  *
- *    * Return: Always 0.
  *
- *     */
+ * Return: Always 0.
+ *
+ **/
 
 void reset_to_98(int *n)
 {
-
 	*n = 98;
-	
-
 }
