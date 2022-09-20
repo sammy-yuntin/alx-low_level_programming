d6df9ba8a2d7ca103479be84f2cbdf9304269b51
sammy-yuntin <yuntinsammy@yahoo.com>
2022-09-20 08:19:37 -0700

    1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - void swap_int(int *a, int *b) swaps the value
+ * of integers
+ * a swaps to an integer
+ * b swaps to another integer
+ * Return : nothing
+ **/
+
+{
+int *xtra;
+*xtra = &a
+a = &b
+*b = &xtra;
+}
